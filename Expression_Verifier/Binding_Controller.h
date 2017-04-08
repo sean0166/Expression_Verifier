85fba9ae3c00d41ec682e5a4cdd6e593d8071783
Sean Eldon Taipala <taipalasean@yahoo.com>
2017-04-08 16:00:06 -0400

    Added Binding_Control Header

--- Expression_Verifier/Binding_Controller.h
+++ Expression_Verifier/Binding_Controller.h
@@ -1,8 +1,24 @@
 #pragma once
+#include <string>
+#include <iostream>
+using namespace std;
 class Binding_Controller
 {
 public:
 	Binding_Controller();
 	~Binding_Controller();
+	virtual void add() const = 0;
+	virtual void mult() const = 0;
+	virtual void sub() const = 0;
+	virtual void exp() const = 0;
+	virtual void compare() const = 0;
+	void reduceExpression();
+
+	string tokenizeInstruction(string);
+
+
+	int linenum_;
+	int scopeIdentifier_;
+
 };
 
